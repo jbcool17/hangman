bab0aafa09ad47e8ee2592a0c64d45b709b62e85
John Brilla <jbrilla17@gmail.com>
2015-10-09 15:26:01 +1100

    init commit

---
+++ main.js
@@ -0,0 +1,62 @@
+console.log('HANGMAN')
+
+// The Hangman Game:
+
+// The game selects a random word from a word list and makes that the secret word
+// The player guesses one letter at a time, trying to figure out what the word is
+// If the player guesses correctly, any instances of that letter are revealed in the secret word
+// If the player guesses incorrectly, they are penalized by taking away a guess
+// If a player reveals all of the letters of the secret word, they win
+// If a player makes 8 incorrect guesses before solving the secret word, they lose
+// To do:
+
+// Plan out your object model. What variables and functions will each object have? How will the different objects communicate with each other?
+// Get the logic to work and make sure you have the game working in the console first before translating it over to update the DOM.
+// Translate your game over so it updates the DOM.
+// Once you get the game working:
+// Implement a "reset" button that will reset the game and choose a new word
+// Implement a "give up" button that will show the solution
+// Are there ways to refactor your code to make it DRYer?
+
+var Words = {
+
+	word1: 'one',
+	word2: 'two',
+	word3: 'three'
+
+}
+
+
+// for (word in Words) {
+// 	console.log(Words[word])
+// }
+
+theWord = Words.word1
+letters = theWord.split('')
+
+
+var Hangman = {
+	checkForLetter: function (letter) {
+
+		if ( letters.indexOf(letter) === -1 || this.guessedLetters.indexOf(letter) >= 0 ) {
+
+			console.log('You already guessed that letter, yo!');
+			this.guessedLetters.push(letter);
+
+		} else {
+
+			this.guessedLetters.push(letter);
+			this.correctGuesses.push(letter);
+			
+		}
+
+		console.log(this.guessedLetters, this.correctGuesses);
+
+		
+		
+	},
+	guessedLetters: [],
+	correctGuesses:[]
+
+
+}
