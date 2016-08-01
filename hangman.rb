ec2480a18b82efcc4a47bd9b0ea18f7f584cae9b
John Brilla <jbrilla17@gmail.com>
2016-08-01 18:01:59 +1000

    Create hangman.rb
    adding rough draft of js to ruby port.

---
+++ hangman.rb
@@ -0,0 +1,92 @@
+
+
+
+module Game
+	class Hangman
+		
+		def initialize
+			word_list = ['one', 'two', 'three', 'four']
+			@word = 'one' # get random word
+			@letters = @word.split(//)
+			@guessed_letters = []
+			@correct_guesses = Array.new(@letters.count)
+			@lives = 8
+			@points = 0
+
+		end
+
+		def check_for_letter(letter)
+			if ( @guessed_letters.index(letter) != nil )
+				puts "You already guessed that letter or it is invalid!"
+				@lives -= 1
+				check_for_loser
+			else
+				if (@letters.index(letter).nil?)
+					# add to guessed letter array
+					@guessed_letters << letter
+					# subract a life
+					@lives -= 1
+					# check_for_loser
+					check_for_loser
+				else
+					length = @word.split(//).select { |l| l == 'd' }.count
+					if (length > 1)
+						# Loop
+						length.times do 
+							adds_correct_letter(letter)
+							@points += 1
+						end
+							
+						# add letter to guessed_letters
+						@guessed_letters << letter
+
+					else
+						# add to guessLetters
+						@guessed_letters << letter
+						# addsCorrectLetter
+						adds_correct_letter (letter)
+						# add point
+						@points += 1
+					end
+
+					# check_for_winner
+					check_for_winner
+				end
+
+			end
+
+			puts "WORD: | GUESS LETTERS: | CORRECT GUESSES: "
+		end
+
+		def adds_correct_letter(letter)
+			@word.length.times do |i|
+				if (@word[i] == letter)
+					@correct_guesses[i] = letter
+				end
+			end
+
+			puts "STATUS: #{@correct_guesses.join('')}"
+		end
+
+		def check_for_winner
+			if ( @points == @word.length )
+				puts 'You Win!'
+				puts "WORD: #{@word}"
+			end
+		end
+
+		def check_for_loser
+			if @lives == 0 then puts "You Lose!" end
+		end
+
+		# def set_word
+
+		# end
+
+		def output_status(message)
+			puts "STATUS: #{message}"
+		end
+
+
+	end
+end
