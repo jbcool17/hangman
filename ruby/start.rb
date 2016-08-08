9d4f501406bd5be5ab805454e965215b8806f52a
John Brilla <jbrilla17@gmail.com>
2016-08-08 16:34:02 +1000

    Update start.rb

--- ruby/start.rb
+++ ruby/start.rb
@@ -23,13 +23,13 @@ puts "Auto Running Game..."
 puts "--------------------"
 game_two.check_for_letter 'a'
 game_two.check_for_letter 'b'
-game_two.check_for_letter 'c'
-game_two.check_for_letter 'o'
-game_two.check_for_letter 'a'
 game_two.check_for_letter 'n'
+game_two.check_for_letter 'n'
+game_two.check_for_letter 't'
+game_two.check_for_letter 'f'
 # game_two.check_for_letter 'e'
 game_two.check_for_letter 'z'
-game_two.check_for_letter 'j'
+game_two.check_for_letter 'h'
 game_two.check_for_letter 'b'
 game_two.check_for_letter 'k'
 
