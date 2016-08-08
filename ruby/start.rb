1abe859b7113ffc70a2de247c59b2a599a4aac7b
John Brilla <jbrilla17@gmail.com>
2016-08-08 17:28:07 +1000

    removing auto play

--- ruby/start.rb
+++ ruby/start.rb
@@ -1,34 +1,16 @@
 require_relative 'hangman'
 
-game_one = Game::Hangman.new
-
-puts "Auto Running Game..."
-puts "--------------------"
-game_one.check_for_letter 'a'
-game_one.check_for_letter 'b'
-game_one.check_for_letter 'c'
-game_one.check_for_letter 'o'
-game_one.check_for_letter 'a'
-game_one.check_for_letter 'n'
-game_one.check_for_letter 'e'
-
-puts "--------------------"
-puts "-------DONE!--------"
-puts "--------------------"
-puts "Starting Game Two"
-puts "Enter..."
-
-game_two = Game::Hangman.new
+game = Game::Hangman.new
 status = true
 while status do
 	print 'Make a guess: '
 	input = gets.chomp[0]
-	game_two.check_for_letter input
+	game.check_for_letter input
 	
-	if (game_two.check_for_winner || game_two.check_for_loser)
+	if (game.check_for_winner || game.check_for_loser)
 		status = false
 	end	
 end
 
 
-puts 'ending'
+puts 'The Game Has Ended.'
