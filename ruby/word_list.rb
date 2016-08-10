ccd47993879b15fbaeac6a6e30daca0288a57362
John Brilla <jbrilla17@gmail.com>
2016-08-10 16:55:00 +1000

    adding json parser to get .json file

--- ruby/word_list.rb
+++ ruby/word_list.rb
@@ -1,3 +1,5 @@
+require 'json'
+
 module Game
 	class WordList
 		def initialize
@@ -10,10 +12,18 @@ module Game
 
 		private
 		def set_word
-			word_list = ['one', 'two', 'three', 'four']
+			word_list = generate_word_list
 			number = rand(0..word_list.count-1)
 
 			return word_list[number]
+		end
+
+		def generate_word_list
+			file = File.read('dictionary.json')
+
+			data_hash = JSON.parse(file)
+
+			return data_hash.collect {|k,v| k.downcase }
 		end
 	end
 
